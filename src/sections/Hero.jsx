3cd85492e1bff7fc592cfc1d9708610f31cce134
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-01-02 17:21:02 +0530

    major update
    this is for hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
 import Button from "../Components/Button";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
 
 const Hero = () => {
   return (
@@ -16,7 +17,7 @@ const Hero = () => {
           <br />
           <span className="text-coral-red inline-block mt-3">Nike </span> Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylies Nike arrivals quality comfort and innovation for your
           active life.
         </p>
@@ -24,9 +25,25 @@ const Hero = () => {
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat, index) => (
             <div key={index}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin  font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="shoes img"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div>
+          {shoes.map((shoe, index) => (
+            <div key={index}></div>
           ))}
         </div>
       </div>
